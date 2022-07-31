243ce6e358797e0386909ac5b911a7ece0c67a53
flportilla <85775228+flportilla@users.noreply.github.com>
2022-07-31 15:49:09 -0500

    Update index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,6 @@
-const validateFields = require('../middlewares/field-validator.js')
-const validateJWT = require('../middlewares/validate-jtw.js')
-const validateRoles = require('../middlewares/validate-role.js')
+const validateFields = require('../middlewares/field-validator')
+const validateJWT = require('../middlewares/validate-jtw')
+const validateRoles = require('../middlewares/validate-role')
 const validateCategory = require('../middlewares/validate-category')
 
 module.exports = {
